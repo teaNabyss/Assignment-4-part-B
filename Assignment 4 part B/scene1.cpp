dfd74fd3bd2bc1ff90a3079af7b83d7a258ab28b
Diana Ryazanova Ryzanova <n01680072@humber.ca>
2025-04-07 21:38:08 -0400

    created stuff

--- Assignment 4 part B/scene1.cpp
+++ Assignment 4 part B/scene1.cpp
@@ -1,1 +1,8 @@
 //scene 1 was created
+
+int scene1() {
+	int yesCount = 0;
+	int noCount = 0;
+	char response;
+
+}
--- Assignment 4 part B/scene2.cpp
+++ Assignment 4 part B/scene2.cpp
@@ -1,1 +1,15 @@
 //scene 2 was created
+#include <iostream>
+
+int scene2() {
+
+	std::cout << "AAAAAAAAAAAAAAAAAA/n";
+	std::cout << "AAAAAAAAAAAAAAAAAA/n";
+
+	std::cout << "AAAAAAAAAAAAAAAAAA/n";
+	std::cout << "AAAAAAAAAAAAAAAAAA/n";
+	std::cout << "AAAAAAAAAAAAAAAAAA/n";
+
+
+
+}
